b803b4f9037ec94c9a2f9667ea14cd80e63efbfb
Kuba <kubaozut@gmail.com>
2026-03-16 13:11:16 +0100

    Configured CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
@@ -11,7 +13,19 @@ async function bootstrap() {
     transform: true,
   }));
 
+  const corsOrigins = configService
+      .get<string>('CORS_ORIGINS')
+      ?.split(',')
+      .map(origin => origin.trim());
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: corsOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+    exposedHeaders: ['Content-Range'],
+  });
+
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
